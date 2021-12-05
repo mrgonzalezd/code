cd681ce2fd968b58365d062676857fe0c5b4929f
frglezd <ragodrone@openmailbox.org>
2021-12-05 15:54:24 -0700

    config changes

--- addTwo.cpp
+++ addTwo.cpp
@@ -7,48 +7,72 @@ struct ListNode {
      ListNode(int x) : val(x), next(nullptr) {}
      ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
- 
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
         int sum = 0;
         ListNode* sumNode = new ListNode;
-        
-        while(l1 && l2 != nullptr){
+        ListNode* head = sumNode;
+
+        while(l1 != nullptr || l2 != nullptr){
+            sum = 0;
             if(carry > 0){ //check whether there's positive numbers to add into sum
-                sum += carry;   
+                sum += carry;
                 carry = 0;
             }
-            if(l1){
+            if(l1 !=nullptr){
                 sum += l1->val;
             }
-            if(l2){
+            if(l2 !=nullptr){
                 sum += l2->val;
             }
             carry = (int) sum/10;
-            
+
+            //creating new node
             ListNode* nextNode = new ListNode;
             ListNode(sumNode->val= sum%10); //assign accumulated sum to the node value
-            sumNode->next = nextNode;
-            sumNode= sumNode->next;
-            
-            sum = 0;
-            
+            if(l1 != nullptr || l2 != nullptr){
+                sumNode->next = nextNode;
+                sumNode= sumNode->next;
+            }
+            if(l1 != nullptr){
             l1= l1->next;
+            }
+            if(l2 != nullptr){
             l2 =l2->next;
+            }
         }
-        
-        return sumNode;
+        if(carry>0){
+            head->next = new ListNode(carry);
+        }
+        return head;
     }
 };
 int main(){
     Solution x;
-    ListNode c(3);
-    ListNode b(4, &c);
-    ListNode l1(2, &b);
-    ListNode e(4);
-    ListNode d(6, &e);
-    ListNode l2(5, &d);
+    /*
+    ListNode f(9);
+    ListNode e(9, &f);
+    ListNode d(9, &e);
+    ListNode c(9, &d);
+    ListNode b(9, &c);
+    ListNode a(9, &b);
+    ListNode l1(9, &a);
+    ListNode i(9);
+    ListNode h(9, &i);
+    ListNode g(9, &h);
+    ListNode l2(9, &g);
+    */
+    
+    ListNode b(3);
+    ListNode a(4, &b);
+    ListNode l1(2, &a);
+
+    ListNode d(4);
+    ListNode c(6, &d);
+    ListNode l2(5, &c);
     x.addTwoNumbers(&l1, &l2);
+
 }
---
+++ maxSubstring.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution{
+    public:
+        int lengthOfLongestSubstring(string &s){
+            int n = s.length();
+            int ans = 0;
+
+            for (int i= 0; i < n; i++){
+                continue;
+            }
+            return ans;
+        }
+};
+int main(){
+    Solution x;
+    string s = "hello";
+    x.lengthOfLongestSubstring(s);
+    return 0;
+}
