7749f8516b0a39b4218e55bc58450e8886658b87
frglezd <ragodrone@openmailbox.org>
2021-12-22 07:51:07 -0700

    .

--- isNumPalindrome.c
+++ isNumPalindrome.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
+
 bool isPalindrome(int x){
-    bool answer;
-    int count;
+    
+    int y=x;
+    bool answer = 1;
+    if(x < 0){
+        answer = 0;
+        return answer;
+    }
+    int count =0;
     do {
-    x /= 10;
+    y /= 10;
     ++count;
-    } while (x != 0);
+    } while (y != 0);
+    int j = count-1;
     char s[count];
-    sprintf(s, %d, x);
-    while(int i= 0; i<count; i++ ){
-        
+    sprintf(s, "%d", x);
+    
+    for(int i= 0; i<count; i++ ){
+        //for(int j=count-1; j>=0; j--){
+            if(s[i] == s[j])
+            {
+                if(j>=-1){
+                j--;
+                }
+                continue;
+            }
+            else{
+                answer = 0;
+            }
+        //}
     }
     
     return answer;
 }
 
 int main(){
-    int x = 10;
+    int x = 101;
     isPalindrome(x);
     return 0;
 }
