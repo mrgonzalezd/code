3ff152ac6bce77a1737f8fc73956c27e47359ded
frglezd <ragodrone@openmailbox.org>
2022-01-24 02:18:39 -0700

    .

--- check.c
+++ check.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 int check( char *ptr);
- 
 
 void check2( char **ptr) {
 char *new_ptr = "Yellow";
 //ptr="Yellow";
 *ptr = new_ptr;
+}
 
+int check3( char **ptr) {
+char *new_ptr = "Yellow";
+//ptr="Yellow";
+*ptr = new_ptr;
+return 0;
 }
 
 int main() {
 char *ptr="Blue";
-check2(&ptr);
+check3(&ptr);
 printf("%s \n",ptr);
 return 0;
 }
